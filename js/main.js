0c9792e38e6d2c16d6ffcf80af5cb474bf0bc732
katkovergina <51058477+katkovergina@users.noreply.github.com>
2020-05-07 21:34:32 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
 
 //авторизация 
 
+'use strict';
 
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
@@ -98,4 +99,112 @@ function checkAuth() {
 }
 
 checkAuth();
-console.log(loginInput.value);
+
+
+//второй день 
+
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+
+//функция которая генерирует карточку 
+
+function createCardRestaurant() {
+    const card = `
+        <a class="card card-restaurant">
+          <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+          <div class="card-text">
+            <div class="card-heading">
+              <h3 class="card-title">Пицца плюс</h3>
+              <span class="card-tag tag">50 мин</span>
+            </div>
+            <div class="card-info">
+              <div class="rating">
+                4.5
+              </div>
+              <div class="price">От 900 ₽</div>
+              <div class="category">Пицца</div>
+            </div>
+          </div>
+        </a>
+      `;
+
+      cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+}
+
+createCardRestaurant();
+
+function createCardGood() {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.insertAdjacentHTML('beforeend', `
+    
+      <img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">Пицца Везувий</h3>
+        </div>
+  
+        <div class="card-info">
+          <div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+            «Халапенье», соус «Тобаско», томаты.
+          </div>
+        </div>
+    
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">545 ₽</strong>
+        </div>
+      </div>
+     
+  `);
+
+  cardsMenu.insertAdjacentElement('beforeend' ,card);
+
+  
+
+}
+
+function openGoods(event) { 
+   const target = event.target;
+   const restaurant = target.closest('.card-restaurant');
+
+   if (restaurant) {
+
+      cardsMenu.textContent = '';
+      containerPromo.classList.add('hide');
+      restaurants.classList.add('hide');
+      menu.classList.remove('hide');
+
+      
+
+      createCardGood();
+      createCardGood();
+      createCardGood();
+   }
+
+   
+}
+
+
+cardsRestaurants.addEventListener('click', openGoods);
+logo.addEventListener('click', function() {
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+
+});
+
+
+
+
+         
+
+
